f474d0830751bc360c52b0096f29dca97b1e62b4
Houda Jawad <houdajawad@gmail.com>
2023-06-12 08:59:54 -0700

    Add fetch call to api file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,9 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error("Oops! We seem to be having some technical issues, please try again later!");
+    }
+    return res.json();
+  })
 }
